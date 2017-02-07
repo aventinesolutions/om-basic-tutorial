90f9f3ab2fda62d7f7fbb183409f8745cd2908f9
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2017-02-07 21:44:53 +0100

    :bear: getting started

--- src/om_basic_tutorial/core.cljs
+++ src/om_basic_tutorial/core.cljs
@@ -8,15 +8,15 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Frimmel was found stoned in the Red Light District"}))
 
 (om/root
   (fn [data owner]
     (reify om/IRender
       (render [_]
         (dom/div nil
-                 (dom/h1 nil (:text data))
-                 (dom/h3 nil "Edit this and watch it change!")))))
+                 (dom/h2 nil (:text data))
+                 (dom/h3 nil "His wife, Bemb, was shocked and horrified")))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
